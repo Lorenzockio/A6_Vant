1e7aa8ec875b62f11beb21cad2999818cf3a0fb9
Lorenzockio <scribblemelon@gmail.com>
2026-04-23 17:29:34 -0700

    Refactor main.js for improved performance and structure

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 import { default as seagulls } from './gulls.js'
 
-const WORKGROUP_SIZE = 64,
-      NUM_AGENTS = 64,
+const WORKGROUP_SIZE = 128,
+      NUM_AGENTS = 256,
       DISPATCH_COUNT = [NUM_AGENTS/WORKGROUP_SIZE,1,1],
-      GRID_SIZE = 4,
+      GRID_SIZE = 1,
       STARTING_AREA = .3
 
 const W = Math.round( window.innerWidth  / GRID_SIZE ),
@@ -52,43 +52,97 @@ fn cs(@builtin(global_invocation_id) cell:vec3u)  {
   let pIndex    = pheromoneIndex( vant.pos );
   let pheromone = pheremones[ pIndex ];
 
-  var forward = vec2f( 0., 1. ); //up
+  var forward = vec2f( 0., 1. ); //down (y starts at top)
   let d = fract( vant.dir );
   if( d == 0.25 ) {
     forward = vec2f( 1., 0. ); //right
   } else if( d == 0.5 ) {
-    forward = vec2f( 0., -1. ); //down
+    forward = vec2f( 0., -1. ); //up
   } else {
     forward = vec2f( -1., 0. ); //left
   }
+  //dir += 0.25 means turn left
+  //dir -= 0.25 means turn right
 
+  //
   let frontIndex  = pheromoneIndex( vant.pos + forward );
   let backIndex  = pheromoneIndex( vant.pos - forward );
-  let rightIndex  = pheromoneIndex( vant.pos + vec2f( forward.y, -forward.x ) );
-  let leftIndex   = pheromoneIndex( vant.pos + vec2f( -forward.y, forward.x ) );
-
-  // if pheromone in fron and not in back 
-  if ( pheremones[ frontIndex ] == 1. && pheremones[ backIndex ] == 0. ) {
-    pheremones[ frontIndex ] = 0.; //flip pheromone flag
-    pheremones[ backIndex ] = 1.;
-  }
-
-  // if pheromones were found
+  let leftIndex  = pheromoneIndex( vant.pos + vec2f( forward.y, -forward.x ) );
+  let rightIndex   = pheromoneIndex( vant.pos + vec2f( -forward.y, forward.x ) );
+  let diagLeftIndex  = pheromoneIndex( vant.pos + forward + vec2f( forward.y, -forward.x ) );
+  let diagRightIndex = pheromoneIndex( vant.pos + forward + vec2f( -forward.y, forward.x ) );
+  let diagBackLeftIndex  = pheromoneIndex( vant.pos - forward + vec2f( forward.y, -forward.x ) );
+  let diagBackRightIndex = pheromoneIndex( vant.pos - forward + vec2f( -forward.y, forward.x ) );
+  
+  let behavior = u32(vant.flag);
   if( pheromone != 0. ) {
-    pheremones[ pIndex ] = 0.;  // set pheromone flag
-    //if ( pheremones[ frontIndex ] == 0. && pheremones[ backIndex ] == 1. ) {
-    //    vant.dir += .5; // turn 180 degrees
-    //} else {
-        vant.dir += select(.25,-.25,vant.flag==0.); // turn 90 degrees 
-    //}
-  }else{
-    pheremones[ pIndex ] = 1.;  // unset pheromone flag
-    if ( pheremones[ frontIndex ] == 0. && pheremones[ backIndex ] == 1. ) {
-        vant.dir += .5; // turn 180 degrees
-    } else {
-        vant.dir += select(-.25,.25,vant.flag==0.); // turn 90 degrees 
+    pheremones[ pIndex ] = 0.;
+    switch(behavior) {
+      case 0u: { vant.dir -= .25; }
+      case 1u: { vant.dir += .25; }
+      case 2u: {vant.dir -= .25;} // look at current pos
+      case 3u: {vant.dir += .25;}
+      //case 2u: {} //look ahead
+      //case 3u: {} //not sure which I prefer 
+      case 4u: { vant.dir -= .25; }
+      case 5u: { vant.dir += .25; }
+      case 6u: {}
+      default: { vant.dir -= .25; }
+    }
+  } else {
+    pheremones[ pIndex ] = 1.;
+    switch(behavior) {
+      case 0u: { vant.dir += .25; }
+      case 1u: { vant.dir -= .25; }
+      case 2u: {}
+      case 3u: {}
+      case 4u: {
+        pheremones[ leftIndex ] = 1.;
+        vant.dir += .25; //turn into left index that was set
+        //eats it and turns right 
+        //effectively turns diagnonal 
+      }
+      case 5u: {
+        pheremones[ rightIndex ] = 1.;
+        vant.dir -= .25; 
+      }
+      case 6u: { pheremones[ pIndex ] = 0.; } //dont leave pheromone 
+      default: { vant.dir += .25; }
+    }
+  } 
+    
+  //not sure which I prefer -> case 2u and 3u
+  /*if (behavior == 2u || behavior == 3u) {
+    // if pheromone in front 
+    if ( pheremones[ frontIndex ] == 1. ) {
+      pheremones[ frontIndex ] = 0.; //eat
+      if (behavior == 2u) {
+        vant.dir -= .25;
+      } else {
+        vant.dir += .25;  
+      }
+    } else {}
+  }*/
+  
+  if (behavior == 6u) { // follows pheremones or goes straight
+    if ( pheremones[ frontIndex ] == 1. ) {} //go straight if pheromone in front
+    else if ( pheremones[ leftIndex ] == 1. ) {
+      vant.dir += .25;
+    } else if ( pheremones[ rightIndex ] == 1. ) {
+      vant.dir -= .25;
+    } else if ( pheremones[ backIndex ] == 1. ) {
+      vant.dir += .5;
+    } else if ( pheremones[ diagLeftIndex ] == 1. ) {
+      vant.dir += .25;
+    } else if ( pheremones[ diagRightIndex ] == 1. ) {
+      vant.dir -= .25;
+    } else if ( pheremones[ diagBackLeftIndex ] == 1. ) {
+      vant.dir += .5;
+    } else if ( pheremones[ diagBackRightIndex ] == 1. ) { //diags nessecary?
+      vant.dir -= .5;
     }
   }
+
 
   // calculate direction based on vant heading
   let dir = vec2f( sin( vant.dir * pi2 ), cos( vant.dir * pi2 ) );
@@ -113,8 +167,8 @@ const offset = .5 - STARTING_AREA / 2
 for( let i = 0; i < NUM_AGENTS * NUM_PROPERTIES; i+= NUM_PROPERTIES ) {
   vants[ i ]   = Math.floor( (offset+Math.random()*STARTING_AREA) * W ) // x
   vants[ i+1 ] = Math.floor( (offset+Math.random()*STARTING_AREA) * H ) // y
-  vants[ i+2 ] = 0 // direction 
-  vants[ i+3 ] = Math.round( Math.random()  ) // vant behavior type 
+  vants[ i+2 ] = Math.round( Math.random() * 3) / 4 // direction (0, .25, .5, .75)
+  vants[ i+3 ] = Math.round( Math.random() * 6) // vant behavior type 
 }
 
 const sg = await seagulls.init()
